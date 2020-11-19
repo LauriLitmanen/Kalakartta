16cade6d6a3adef2c6e816a89f08656280fbbf8b
LauriLitmanen <litmanen.lauri@gmail.com>
2020-11-19 18:36:38 +0200

    modeled DB, set up mongoose, POST and GET methods

---
+++ server/src/api/reports.js
@@ -0,0 +1,33 @@
+const { Router } = require('express');
+
+const ReportEntry = require('../models/CatchReport')
+
+const router = Router();
+
+router.get('/', async (req, res, next) => {
+    try {
+        const entries = await ReportEntry.find();
+        res.json(entries);
+    } catch (error) {
+        next(error);
+    }
+  
+});
+
+router.post('/', async (req, res, next) => {
+    try {
+        const reportEntry = new ReportEntry(req.body);
+        const createdEntry = await reportEntry.save();
+        res.json(createdEntry);
+    } catch (error) {
+        console.log(error.constructor.name);
+        if (error.name === 'ValidationError'){
+            res.status(422);
+        }
+        next(error);
+    }
+}); 
+
+
+
+module.exports = router;
---
+++ server/src/models/CatchReport.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const { Schema }= mongoose;
+
+const requiredString = {
+    type: String,
+    required: true
+};
+
+const requiredNumber = {
+    type: Number,
+    required: true
+};
+
+const catchReportSchema = new Schema ({
+    title: requiredString,
+    species: requiredString,
+    length: {
+        type: Number,
+        min: [0, 'A bit small eh?'],
+        max: [1000, 'a whale?']
+    },
+    weight: {
+        type: Number,
+        min: [0, 'A bit small eh?'],
+        max: [300, 'Thats a pig!']
+    },
+    fishingMethod: requiredString,
+    lure: String,
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    catchPhoto: String,
+    latitude: {
+    ...requiredNumber,
+    min: -90,
+    max: 90,
+    },
+    longitude: {
+    ...requiredNumber,
+    min: -180,
+    max: 180,
+    } ,
+}, {
+    timestamps: true,
+});
+
+const catchReport = mongoose.model('CatchReport', catchReportSchema);
+
+module.exports = catchReport;
